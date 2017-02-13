1eae579e11ad781ccc4f401503a7abcc8ca06938
J. Iván Alegre <IvanAlegre@users.noreply.github.com>
2017-02-13 18:59:32 +0100

    Add custom metrics to new version. Keep backwards comp

--- advanced-example.js
+++ advanced-example.js
@@ -1,38 +1,44 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const promBundle = require("express-prom-bundle");
+const promBundle = require('express-prom-bundle');
+
+// here we want to remove default metrics provided in prom-client
+// this must be done before initializing promBundle
+clearInterval(promBundle.promClient.defaultMetrics());
+promBundle.promClient.register.clear();
 
 const bundle = promBundle({
-    prefix: "demo_app:something:",
-    blacklist: [/up/],
-    buckets: [0.1, 0.4, 0.7]
+  blacklist: [/up/],
+  buckets: [0.1, 0.4, 0.7],
+  includeMethod: true,
+  includePath: true
 });
 
 app.use(bundle);
 
 // native prom-client metric (no prefix)
-const c1 = new bundle.promClient.Counter("c1", "c1 help");
+const c1 = new bundle.promClient.Counter('c1', 'c1 help');
 c1.inc(10);
 
-// create metric using factory (w/ prefix)
-const c2 = bundle.factory.newCounter("c2", "c2 help");
-c2.inc(20);
-
-app.get("/foo", (req, res) => {
-    setTimeout(() => {
-        res.send("foo response\n");
-    }, 500);
+app.get('/foo/:id', (req, res) => {
+  setTimeout(() => {
+    res.send('foo response\n');
+  }, 500);
 });
-app.get("/bar", (req, res) => res.send("bar response\n"));
+app.delete('/foo/:id', (req, res) => {
+  setTimeout(() => {
+    res.send('foo deleted\n');
+  }, 300);
+});
+app.get('/bar', (req, res) => res.send('bar response\n'));
 
-app.listen(3000, () => console.log("listening on 3000")); // eslint-disable-line
-
-/*
-test in shell console:
-
-curl localhost:3000/foo
-curl localhost:3000/bar
-curl localhost:3000/metrics
-*/
+app.listen(3000, () => console.info(  // eslint-disable-line
+  'listening on 3000\n'
+  + 'test in shell console\n\n'
+  + 'curl localhost:3000/foo/1234\n'
+  + 'curl -X DELETE localhost:3000/foo/5432\n'
+  + 'curl localhost:3000/bar\n'
+  + 'curl localhost:3000/metrics\n'
+));
--- src/index.js
+++ src/index.js
@@ -1,195 +1,195 @@
-"use strict";
-
-const
-    PromFactory = require("./PromFactory"),
-    onFinished = require("on-finished"),
-    pidusage = require('pidusage'),
-    os = require("os");
+'use strict';
+const onFinished = require('on-finished');
+const promClient = require('prom-client');
+const normalizePath = require('./normalizePath');
+const pidusage = require('pidusage');
+const os = require('os');
 
 function matchVsRegExps(element, regexps) {
-    for (let regexp of regexps) {
-        if (regexp instanceof RegExp) {
-            if (element.match(regexp)) {
-                return true;
-            }
-        } else if (element == regexp) {
-            return true;
-        }
+  for (let regexp of regexps) {
+    if (regexp instanceof RegExp) {
+      if (element.match(regexp)) {
+        return true;
+      }
+    } else if (element === regexp) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
-
 function filterArrayByRegExps(array, regexps) {
-    return array.filter(element => {
-        return matchVsRegExps(element, regexps);
-    });
+  return array.filter(element => {
+    return matchVsRegExps(element, regexps);
+  });
 }
 
 function prepareMetricNames(opts, metricTemplates) {
-    const names = Object.keys(metricTemplates);
-    if (opts.whitelist) {
-        if (opts.blacklist) {
-            throw new Error("you cannot have whitelist and blacklist at the same time");
-        }
-        return filterArrayByRegExps(names, opts.whitelist);
-    }
+  const names = Object.keys(metricTemplates);
+  if (opts.whitelist) {
     if (opts.blacklist) {
-        const blacklisted = filterArrayByRegExps(names, opts.blacklist);
-        return names.filter(name => blacklisted.indexOf(name) === -1);
+      throw new Error('you cannot have whitelist and blacklist at the same time');
     }
-    return names;
+    return filterArrayByRegExps(names, opts.whitelist);
+  }
+  if (opts.blacklist) {
+    const blacklisted = filterArrayByRegExps(names, opts.blacklist);
+    return names.filter(name => blacklisted.indexOf(name) === -1);
+  }
+  return names;
 }
 
 function main(opts) {
-    opts = opts === undefined ? {} : opts;
-    if (arguments[2] && arguments[1] && arguments[1].send) {
-        arguments[1].status(500)
-            .send("<h1>500 Error</h1>\n"
-                + "<p>Unexapected 3d param in express-prom-bundle.\n"
-                + "<p>Did you just put express-prom-bundle into app.use "
-                + "without calling it as a function first?");
-        return;
-    }
+  opts = Object.assign({autoregister: true}, opts);
+  if (arguments[2] && arguments[1] && arguments[1].send) {
+    arguments[1].status(500)
+      .send('<h1>500 Error</h1>\n'
+        + '<p>Unexpected 3rd param in express-prom-bundle.\n'
+        + '<p>Did you just put express-prom-bundle into app.use '
+        + 'without calling it as a function first?');
+    return;
+  }
 
-    const factory = new PromFactory(opts);
+  if (opts.prefix || opts.keepDefaultMetrics !== undefined) {
+    throw new Error(
+      'express-prom-bundle detected obsolete options:'
+      + 'prefix and/or keepDefaultMetrics. '
+      + 'Please refer to oficial docs. '
+      + 'Most likely you upgraded the module without necessary code changes'
+    );
+  }
 
-    const metricTemplates = {
-        "up": () => factory.newGauge(
-            "up",
-            "1 = up, 0 = not up"
-        ),
-        "nodejs_memory_heap_total_bytes": () => factory.newGauge(
-            "nodejs_memory_heap_total_bytes",
-            "value of process.memoryUsage().heapTotal"
-        ),
-        "nodejs_memory_heap_used_bytes": () => factory.newGauge(
-            "nodejs_memory_heap_used_bytes",
-            "value of process.memoryUsage().heapUsed"
-        ),
-        "http_request_seconds": () => {
-            const metric = factory.newHistogram(
-                "http_request_seconds",
-                "number of http responses labeled with status code",
-                ["status_code"],
-                {
-                    buckets: opts.buckets || [0.003, 0.03, 0.1, 0.3, 1.5, 10]
-                }
-            );
-            return metric;
-        },
-        "nodejs_cpu": () => factory.newGauge(
-            "nodejs_cpu",
-            "cpu usage"
-        ),
-        "nodejs_memory": () => factory.newGauge(
-            "nodejs_memory",
-            "memory usage"
-        ),
-        "nodejs_load1": () => factory.newGauge(
-            "nodejs_load1",
-            "average 1-minute load"
-        ),
-        "nodejs_load5": () => factory.newGauge(
-            "nodejs_load5",
-            "average 5-minutes load"
-        ),
-        "nodejs_load15": () => factory.newGauge(
-            "nodejs_load15",
-            "average 15-minutes load"
-        ),
-    };
+  const httpMetricName = opts.httpDurationMetricName || 'http_request_seconds';
 
-    const
-        metrics = {},
-        dynmetrics = [],
-        names = prepareMetricNames(opts, metricTemplates);
-
-
-    for (let name of names) {
-        metrics[name] = metricTemplates[name]();
-    }
-
-    if (metrics.up) {
-        metrics.up.set(1);
-    }
-
-    const middleware = function (req, res, next) {
-        if (req.path == "/metrics") {
-            let memoryUsage = process.memoryUsage();
-            if (metrics["nodejs_memory_heap_total_bytes"]) {
-                metrics["nodejs_memory_heap_total_bytes"].set(memoryUsage.heapTotal);
-            }
-            if (metrics["nodejs_memory_heap_used_bytes"]) {
-                metrics["nodejs_memory_heap_used_bytes"].set(memoryUsage.heapUsed);
-            }
-            if (metrics["nodejs_cpu"] || metrics["nodejs_memory"]) {
-                pidusage.stat(process.pid, (err, stat) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        // console.log(stat);
-                        if (metrics["nodejs_cpu"]) {
-                            metrics["nodejs_cpu"].set(stat.cpu);
-                        }
-                        if (metrics["nodejs_memory"]) {
-                            metrics["nodejs_memory"].set(stat.memory / 1024 / 1024);
-                        }
-                    }
-                });
-                // pidusage.unmonitor(process.pid);
-            }
-            if (metrics["nodejs_load1"] || metrics["nodejs_load5"] || metrics["nodejs_load15"]) {
-                const load = os.loadavg();
-                const load1 = load[0];
-                const load5 = load[1];
-                const load15 = load[2];
-                if (metrics["nodejs_load1"]) {
-                    metrics["nodejs_load1"].set(load1);
-                }
-                if (metrics["nodejs_load5"]) {
-                    metrics["nodejs_load5"].set(load5);
-                }
-                if (metrics["nodejs_load15"]) {
-                    metrics["nodejs_load15"].set(load15);
-                }
-            }
-            dynmetrics.forEach(([gauge, updater]) => {
-              gauge.set(updater());
-            });
-            res.contentType("text/plain")
-                .send(factory.promClient.register.metrics());
-            return;
+  const metricTemplates = {
+    'up': () => new promClient.Gauge(
+      'up',
+      '1 = up, 0 = not up'
+    ),
+    http_request_duration_seconds: () => {
+      const labels = ['status_code'];
+      if (opts.includeMethod) {
+        labels.push('method');
+      }
+      if (opts.includePath) {
+        labels.push('path');
+      }
+      const metric = new promClient.Histogram(
+        httpMetricName,
+        'duration histogram of http responses labeled with: ' + labels.join(', '),
+        labels,
+        {
+          buckets: opts.buckets || [0.003, 0.03, 0.1, 0.3, 1.5, 10]
         }
+      );
+      return metric;
+    },
+    'nodejs_cpu': () => new promClient.Gauge(
+      'nodejs_cpu',
+      'cpu usage'
+    ),
+    'nodejs_memory': () => new promClient.Gauge(
+      'nodejs_memory',
+      'memory usage'
+    ),
+    'nodejs_load1': () => new promClient.Gauge(
+      'nodejs_load1',
+      'average 1-minute load'
+    ),
+    'nodejs_load5': () => new promClient.Gauge(
+      'nodejs_load5',
+      'average 5-minute load'
+    ),
+    'nodejs_load15': () => new promClient.Gauge(
+      'nodejs_load15',
+      'average 15-minute load'
+    ),
+  };
 
-        if (opts.excludeRoutes && matchVsRegExps(req.path, opts.excludeRoutes)) {
-            return next();
-        }
+  const metrics = {};
+  const names = prepareMetricNames(opts, metricTemplates);
 
-        let labels;
-        if (metrics["http_request_seconds"]) {
-            labels = {"status_code": 0};
-            let timer = metrics["http_request_seconds"].startTimer(labels);
-            onFinished(res, () => {
-                labels["status_code"] = res.statusCode;
-                timer();
-            });
-        }
+  for (let name of names) {
+    metrics[name] = metricTemplates[name]();
+  }
 
-        next();
-    };
+  if (metrics.up) {
+    metrics.up.set(1);
+  }
 
-    function addMetric(name, description, f) {
-      dynmetrics.push([factory.newGauge(name, description), f]);
+  const metricsMiddleware = function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(promClient.register.metrics());
+  };
+
+  const middleware = function (req, res, next) {
+    const path = req.originalUrl;
+    let labels;
+
+    if (opts.autoregister && path.match(/^\/metrics\/?$/)) {
+      return metricsMiddleware(req, res);
     }
 
-    middleware.factory = factory;
-    middleware.metricTemplates = metricTemplates;
-    middleware.metrics = metrics;
-    middleware.promClient = factory.promClient;
-    middleware.addMetric = addMetric;
+    if (opts.excludeRoutes && matchVsRegExps(path, opts.excludeRoutes)) {
+      return next();
+    }
 
-    return middleware;
+    if (metrics.http_request_duration_seconds) {
+      labels = {'status_code': 0};
+      let timer = metrics.http_request_duration_seconds.startTimer(labels);
+      onFinished(res, () => {
+        labels.status_code = res.statusCode;
+        if (opts.includeMethod) {
+          labels.method = req.method;
+        }
+        if (opts.includePath) {
+          labels.path = main.normalizePath(req, opts);
+        }
+        timer();
+      });
+    }
+    if (metrics['nodejs_cpu'] || metrics['nodejs_memory']) {
+      pidusage.stat(process.pid, (err, stat) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // console.log(stat);
+          if (metrics['nodejs_cpu']) {
+            metrics['nodejs_cpu'].set(stat.cpu);
+          }
+          if (metrics['nodejs_memory']) {
+            metrics['nodejs_memory'].set(stat.memory / 1024 / 1024);
+          }
+        }
+      });
+      // pidusage.unmonitor(process.pid);
+    }
+    if (metrics['nodejs_load1'] || metrics['nodejs_load5'] || metrics['nodejs_load15']) {
+      const load = os.loadavg();
+      const load1 = load[0];
+      const load5 = load[1];
+      const load15 = load[2];
+      if (metrics['nodejs_load1']) {
+        metrics['nodejs_load1'].set(load1);
+      }
+      if (metrics['nodejs_load5']) {
+        metrics['nodejs_load5'].set(load5);
+      }
+      if (metrics['nodejs_load15']) {
+        metrics['nodejs_load15'].set(load15);
+      }
+    }
+
+    next();
+  };
+
+  middleware.metricTemplates = metricTemplates;
+  middleware.metrics = metrics;
+  middleware.promClient = promClient;
+  middleware.metricsMiddleware = metricsMiddleware;
+  return middleware;
 }
 
+main.promClient = promClient;
+main.normalizePath = normalizePath;
 module.exports = main;
